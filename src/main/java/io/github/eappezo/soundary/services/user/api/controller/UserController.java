c6a321282bd4af00af698d5057fde76d76e94149
young-in <hardworkingini@gmail.com>
2024-09-25 09:47:12 +0900

    feat : user api만들기 위한 기본 setting

---
+++ src/main/java/io/github/eappezo/soundary/services/user/api/controller/UserController.java
@@ -0,0 +1,9 @@
+package io.github.eappezo.soundary.services.user.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+}
---
+++ src/main/java/io/github/eappezo/soundary/services/user/application/service/UserService.java
@@ -0,0 +1,7 @@
+package io.github.eappezo.soundary.services.user.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}

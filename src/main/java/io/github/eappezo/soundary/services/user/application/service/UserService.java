b3c0a037889c6123e8063269402f45bfbcfae87d
young-in <hardworkingini@gmail.com>
2024-09-27 14:24:00 +0900

    feat : User 정보 업데이트

--- src/main/java/io/github/eappezo/soundary/services/user/application/service/UserService.java
+++ src/main/java/io/github/eappezo/soundary/services/user/application/service/UserService.java
@@ -5,6 +5,8 @@ import io.github.eappezo.soundary.core.user.User;
 import io.github.eappezo.soundary.core.user.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -18,4 +20,22 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
     }
 
+    // 이전에 짜둔거라 수정해야함 identifier 사용하는거 아직 제대로 안 봄
+    public User updateUser(Identifier userId, UserUpdateRequest request) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            User updatedUser = new User(
+                    user.getIdentifier(),
+                    user.getDisplayId(),
+                    request.description(),
+                    request.nickname(),
+                    request.profileImageUrl(),
+                    user.getRoles(),
+                    user.getSignupAt()
+            );
+            return userRepository.save(updatedUser);
+        }
+        return null;
+    }
 }

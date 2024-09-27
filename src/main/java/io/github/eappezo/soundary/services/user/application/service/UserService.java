e53340beb0337cae32a7de1bf9dca03388135c8b
young-in <hardworkingini@gmail.com>
2024-09-27 14:09:31 +0900

    feat : UserService 틀 생성

--- src/main/java/io/github/eappezo/soundary/services/user/application/service/UserService.java
+++ src/main/java/io/github/eappezo/soundary/services/user/application/service/UserService.java
@@ -1,7 +1,21 @@
 package io.github.eappezo.soundary.services.user.application.service;
 
+import io.github.eappezo.soundary.core.identification.Identifier;
+import io.github.eappezo.soundary.core.user.User;
+import io.github.eappezo.soundary.core.user.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserInfo(Identifier userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+    }
+
 }

5ffe059093809f4d6a23d49b06c6060e2f49c759
young-in <hardworkingini@gmail.com>
2024-09-27 14:09:22 +0900

    feat : userController 틀 생성

--- src/main/java/io/github/eappezo/soundary/services/user/api/controller/UserController.java
+++ src/main/java/io/github/eappezo/soundary/services/user/api/controller/UserController.java
@@ -1,9 +1,12 @@
 package io.github.eappezo.soundary.services.user.api.controller;
 
+import io.github.eappezo.soundary.services.user.application.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
 }

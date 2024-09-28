d38600d49c0cdb59cd3c06e4bc5e4748e45b380b
Gyumin Bae <bgmsound0528@gmail.com>
2024-09-28 18:33:13 +0900

    refactor: 패키지 구조 변경

--- src/main/java/io/github/eappezo/soundary/services/music/endpoint/api/controller/TrackSearchController.java
+++ src/main/java/io/github/eappezo/soundary/services/music/endpoint/api/controller/TrackSearchController.java
@@ -1,8 +1,9 @@
-package io.github.eappezo.soundary.services.music.endpoint.api;
+package io.github.eappezo.soundary.services.music.endpoint.api.controller;
 
 import io.github.eappezo.soundary.services.music.application.service.TrackSearchService;
 import io.github.eappezo.soundary.services.music.domain.MusicPlatform;
 import io.github.eappezo.soundary.services.music.domain.Track;
+import io.github.eappezo.soundary.services.music.endpoint.api.TrackSearchAPI;
 import io.github.eappezo.soundary.services.music.endpoint.api.dto.SearchTrackResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;

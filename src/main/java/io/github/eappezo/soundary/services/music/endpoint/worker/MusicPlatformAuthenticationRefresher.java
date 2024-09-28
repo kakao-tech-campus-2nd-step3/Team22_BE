ef4123bea309830ce8f6cdcfca60c6c7e9944c64
Gyumin Bae <bgmsound0528@gmail.com>
2024-09-28 18:18:09 +0900

    refactor: 인증 키 refresh 시 비동기로 수행하도록 변경

--- src/main/java/io/github/eappezo/soundary/services/music/endpoint/worker/MusicPlatformAuthenticationRefresher.java
+++ src/main/java/io/github/eappezo/soundary/services/music/endpoint/worker/MusicPlatformAuthenticationRefresher.java
@@ -3,6 +3,7 @@ package io.github.eappezo.soundary.services.music.endpoint.worker;
 import io.github.eappezo.soundary.services.music.application.service.MusicPlatformAuthenticationRefreshService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class MusicPlatformAuthenticationRefresher {
     private final MusicPlatformAuthenticationRefreshService refreshService;
 
+    @Async
     @Scheduled(fixedDelay = 55 * 60 * 1000)
     public void refreshAll() {
         refreshService.refreshAll();

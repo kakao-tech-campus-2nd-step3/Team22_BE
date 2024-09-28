d38dc166f8b23366920bd7e18b1af5ec73458e5c
Gyumin Bae <bgmsound0528@gmail.com>
2024-09-28 17:49:55 +0900

    docs: 음악 검색용 docs 추가

--- src/main/java/io/github/eappezo/soundary/services/music/endpoint/api/TrackSearchAPI.java
+++ src/main/java/io/github/eappezo/soundary/services/music/endpoint/api/TrackSearchAPI.java
@@ -1,11 +1,18 @@
 package io.github.eappezo.soundary.services.music.endpoint.api;
 
 import io.github.eappezo.soundary.services.music.endpoint.api.dto.SearchTrackResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "음악 검색 API", description = "음악을 조회합니다.")
 public interface TrackSearchAPI {
 
+    @Operation(summary = "트랙 검색하기", description = "트랙을 검색합니다.")
+    @ApiResponse(responseCode = "200", description = "검색 결과")
     SearchTrackResponse search(String platform, String query);
 
 }

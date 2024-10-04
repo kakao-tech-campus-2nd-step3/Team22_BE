3402f90a36a70ec457145e3546735c96032ca31c
Gyumin Bae <bgmsound0528@gmail.com>
2024-10-05 01:21:23 +0900

    refactor: batch arg list를 array list로 변경

--- src/main/java/io/github/eappezo/soundary/services/music/infrastructure/persistence/TrackBatchUpserterImpl.java
+++ src/main/java/io/github/eappezo/soundary/services/music/infrastructure/persistence/TrackBatchUpserterImpl.java
@@ -7,7 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -19,12 +19,12 @@ public class TrackBatchUpserterImpl implements TrackBatchUpserter {
     @Transactional
     public void upsert(List<SimpleTrackDto> tracks) {
         String sql = """
-                        INSERT INTO `tracks` (platform, platform_track_id, title, album_title, serialized_artists, album_cover_url, preview_mp3_url, duration_in_seconds)
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-                        ON DUPLICATE KEY UPDATE title = VALUES(title), album_title = VALUES(album_title), serialized_artists = VALUES(serialized_artists),
-                        album_cover_url = VALUES(album_cover_url), preview_mp3_url = VALUES(preview_mp3_url), duration_in_seconds = VALUES(duration_in_seconds)
-                     """;
-        List<Object[]> batchArgs = new LinkedList<>();
+                   INSERT INTO `tracks` (platform, platform_track_id, title, album_title, serialized_artists, album_cover_url, preview_mp3_url, duration_in_seconds)
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                   ON DUPLICATE KEY UPDATE title = VALUES(title), album_title = VALUES(album_title), serialized_artists = VALUES(serialized_artists),
+                   album_cover_url = VALUES(album_cover_url), preview_mp3_url = VALUES(preview_mp3_url), duration_in_seconds = VALUES(duration_in_seconds)
+                """;
+        List<Object[]> batchArgs = new ArrayList<>();
         List<TrackEntity> trackEntities = tracks.stream().map(TrackEntity::from).toList();
         for (TrackEntity track : trackEntities) {
             Object[] values = {

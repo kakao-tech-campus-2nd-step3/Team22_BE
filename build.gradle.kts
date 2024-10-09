7799289b91098a0d6296d3888008f648c56885d4
Gyumin Bae <bgmsound0528@gmail.com>
2024-10-09 18:08:18 +0900

    refactor: kafka 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.kafka:spring-kafka")
 
     // libraries
     compileOnly("org.projectlombok:lombok")

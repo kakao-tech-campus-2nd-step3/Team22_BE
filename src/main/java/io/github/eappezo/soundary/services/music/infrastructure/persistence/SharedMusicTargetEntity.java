c98faa4b55c10df1f956fddf33971aa3e4950a89
Gyumin Bae <bgmsound0528@gmail.com>
2024-10-10 00:58:09 +0900

    refactor: composite primary key 에 equals & hash code 추가

--- src/main/java/io/github/eappezo/soundary/services/music/infrastructure/persistence/SharedMusicTargetEntity.java
+++ src/main/java/io/github/eappezo/soundary/services/music/infrastructure/persistence/SharedMusicTargetEntity.java
@@ -3,16 +3,14 @@ package io.github.eappezo.soundary.services.music.infrastructure.persistence;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity(name = "shared_music_target")
 @IdClass(SharedMusicTargetEntity.class)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class SharedMusicTargetEntity {
     @Id
     private String sharedMusicId;
--- src/main/java/io/github/eappezo/soundary/services/user/infrastructure/persistence/entity/key/UserRoleEntityKey.java
+++ src/main/java/io/github/eappezo/soundary/services/user/infrastructure/persistence/entity/key/UserRoleEntityKey.java
@@ -3,12 +3,14 @@ package io.github.eappezo.soundary.services.user.infrastructure.persistence.enti
 import io.github.eappezo.soundary.core.identification.Identifier;
 import io.github.eappezo.soundary.core.user.UserRole;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class UserRoleEntityKey {
     private String userId = "";
     private UserRole role = UserRole.PENDING;

973dcce16c86e5f6c90840b48ed77e2a67bb7dbb
young-in <hardworkingini@gmail.com>
2024-10-10 16:05:06 +0900

    feat : 푸시 알림 전송 api 구현

--- src/main/java/io/github/eappezo/soundary/services/fcm/api/controller/FCMController.java
+++ src/main/java/io/github/eappezo/soundary/services/fcm/api/controller/FCMController.java
@@ -1,5 +1,6 @@
 package io.github.eappezo.soundary.services.fcm.api.controller;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import io.github.eappezo.soundary.core.authentication.AuthenticatedUser;
 import io.github.eappezo.soundary.core.identification.Identifier;
 import io.github.eappezo.soundary.services.fcm.api.dto.PostTokenReq;
@@ -12,11 +13,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/pushAlarm")
 @RequiredArgsConstructor
-public class FCMController {
+public class FCMController implements FCMAPI{
     private final FCMService fcmService;
 
+    @Override
     @PostMapping("/token")
     public ResponseEntity<String> getToken(@AuthenticatedUser Identifier userId, @Valid @RequestBody PostTokenReq token){
         return ResponseEntity.ok(fcmService.getToken(userId, token.getToken()));
+    }
+
+    @Override
+    @PostMapping("/send")
+    public ResponseEntity<String> sendPushNotification(@AuthenticatedUser Identifier userId, @Valid @RequestBody PostTokenReq token, @RequestParam String title, @RequestParam String body) throws FirebaseMessagingException {
+        fcmService.sendMessage(token.getToken(), title, body);
+        return ResponseEntity.ok("푸시 알림 전송 성공");
     }
 }

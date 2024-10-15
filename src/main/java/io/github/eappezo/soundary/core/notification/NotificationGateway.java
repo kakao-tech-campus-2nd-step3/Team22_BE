d097bc6798e71c6e5a58bb49282f0f8fbef7bb1d
young-in <hardworkingini@gmail.com>
2024-10-15 17:24:36 +0900

    feat : device 등록 추가

--- src/main/java/io/github/eappezo/soundary/core/notification/NotificationGateway.java
+++ src/main/java/io/github/eappezo/soundary/core/notification/NotificationGateway.java
@@ -3,5 +3,5 @@ package io.github.eappezo.soundary.core.notification;
 public interface NotificationGateway {
 
     void notice(Notification notification);
-
+    void registerDevice(String userId, String fcmToken);
 }

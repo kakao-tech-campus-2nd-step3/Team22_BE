aee784fc6bbdf85149ce5c899ee3734e62acfd48
young-in <hardworkingini@gmail.com>
2024-10-15 13:45:43 +0900

    feat : 사용하지 않는 코드 삭제

--- src/main/java/io/github/eappezo/soundary/core/pushAlarm/PushAlarmOperationGateway.java
+++
@@ -1,6 +0,0 @@
-package io.github.eappezo.soundary.core.pushAlarm;
-
-@FunctionalInterface
-public interface PushAlarmOperationGateway {
-    <T> T excuteOperation(PushAlarmOperation<T> operation);
-}

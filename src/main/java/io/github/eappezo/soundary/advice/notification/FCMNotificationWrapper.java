8f5fab837798834ddd8cc8d23e3473393c6efaa3
young-in <hardworkingini@gmail.com>
2024-10-16 15:03:41 +0900

    refactor : message 내 값들 return
    getNotification()이 Message 클래스에서 public이 아니어서 접근이 불가능해서, Message 객체를 만들 때 알림 관련 데이터를 FCMNotificationWrapper에 전달하여 저장하고 사용하도록 수정

--- src/main/java/io/github/eappezo/soundary/advice/notification/FCMNotificationWrapper.java
+++ src/main/java/io/github/eappezo/soundary/advice/notification/FCMNotificationWrapper.java
@@ -3,11 +3,20 @@ package io.github.eappezo.soundary.advice.notification;
 import com.google.firebase.messaging.Message;
 import io.github.eappezo.soundary.core.notification.Notification;
 
+import com.google.firebase.messaging.Message;
+import io.github.eappezo.soundary.core.notification.Notification;
+
 public class FCMNotificationWrapper implements Notification {
     private final Message message;
+    private final String title;
+    private final String body;
+    private final String targetToken;
 
-    public FCMNotificationWrapper(Message message) {
+    public FCMNotificationWrapper(Message message, String title, String body, String targetToken) {
         this.message = message;
+        this.title = title;
+        this.body = body;
+        this.targetToken = targetToken;
     }
 
     public Message message() {
@@ -16,16 +25,16 @@ public class FCMNotificationWrapper implements Notification {
 
     @Override
     public String getTitle() {
-        return "";
+        return title;
     }
 
     @Override
     public String getBody() {
-        return "";
+        return body;
     }
 
     @Override
     public String getTargetToken() {
-        return "";
+        return targetToken;
     }
 }

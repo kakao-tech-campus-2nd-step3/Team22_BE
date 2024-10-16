a9af4fc23d1a8de16813ac3eccff5be13d2dd3a7
young-in <hardworkingini@gmail.com>
2024-10-16 15:28:24 +0900

    refactor : servie단에 있을 코드가 아니라서 삭제

--- src/main/java/io/github/eappezo/soundary/services/user/application/service/UserService.java
+++ src/main/java/io/github/eappezo/soundary/services/user/application/service/UserService.java
@@ -43,11 +43,4 @@ public class UserService {
         return userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
     }
 
-    public UserRole getUserRole(Identifier userId) {
-        List<UserRole> roles = userRoleManagerImpl.getRolesOf(userId);
-        return roles.stream()
-                .max((role1, role2) -> Integer.compare(role1.getPriority(), role2.getPriority()))
-                .orElse(null);
-    }
-
 }

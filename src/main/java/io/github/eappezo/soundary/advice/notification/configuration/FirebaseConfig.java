994465460c7a0fe278a0900471d9510df5d1eba4
Gyumin Bae <bgmsound0528@gmail.com>
2024-10-18 21:35:30 +0900

    refactor: 코드 클린업

--- src/main/java/io/github/eappezo/soundary/advice/notification/configuration/FirebaseConfig.java
+++ src/main/java/io/github/eappezo/soundary/advice/notification/configuration/FirebaseConfig.java
@@ -3,8 +3,6 @@ package io.github.eappezo.soundary.advice.notification.configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import io.github.eappezo.soundary.core.exception.APIException;
-import io.github.eappezo.soundary.core.exception.common.CommonErrorCode;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
@@ -12,24 +10,26 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.InputStream;
 
+import static io.github.eappezo.soundary.core.ExceptionUtil.stackTraceOf;
+
 @Slf4j
 @Configuration
 public class FirebaseConfig {
     @PostConstruct
-    public void init(){
-        try{
+    public void init() {
+        try {
             InputStream serviceAccount = new ClassPathResource("soundaryFCMKey.json").getInputStream();
-            FirebaseOptions options = new FirebaseOptions.Builder()
+            FirebaseOptions options = FirebaseOptions
+                    .builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) { // FirebaseApp이 이미 초기화되어 있지 않은 경우에만 초기화 실행
                 FirebaseApp.initializeApp(options);
             }
-        } catch (Exception e){
-            // 수정하기 ex) APIExceptionHandler
-            log.error("Firebase 초기화에 실패하였습니다.", e);
-            throw new APIException(CommonErrorCode.FIREBASE_INIT_ERROR, "Firebase 초기화에 실패하였습니다.");
+        } catch (Exception e) {
+            log.error("Firebase 초기화에 실패하였습니다.\n : {}", stackTraceOf(e));
+            throw new RuntimeException(e);
         }
     }
 }

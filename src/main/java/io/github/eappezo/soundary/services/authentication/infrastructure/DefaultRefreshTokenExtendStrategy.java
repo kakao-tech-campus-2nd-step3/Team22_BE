00054b9f8604d39ecc522779f948219fd6950f0d
Gyumin Bae <bgmsound0528@gmail.com>
2024-10-18 12:26:06 +0900

    refactor: 연장 판별을 잔여 % 기준으로 변경

--- src/main/java/io/github/eappezo/soundary/services/authentication/infrastructure/DefaultRefreshTokenExtendStrategy.java
+++ src/main/java/io/github/eappezo/soundary/services/authentication/infrastructure/DefaultRefreshTokenExtendStrategy.java
@@ -3,7 +3,6 @@ package io.github.eappezo.soundary.services.authentication.infrastructure;
 import io.github.eappezo.soundary.services.authentication.application.RefreshTokenDto;
 import io.github.eappezo.soundary.services.authentication.application.RefreshTokenExtendStrategy;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +10,10 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 @Component
-@Slf4j
 @RequiredArgsConstructor
 public class DefaultRefreshTokenExtendStrategy implements RefreshTokenExtendStrategy {
-    @Value("${token.refresh-token-extend-threshold}") private double extendThreshold;
+    @Value("${token.refresh-token-extend-threshold}")
+    private double extendThreshold;
     private final JwtProperties jwtProperties;
 
     @Override
@@ -24,8 +23,7 @@ public class DefaultRefreshTokenExtendStrategy implements RefreshTokenExtendStra
                 .refreshedAt()
                 .until(now, ChronoUnit.SECONDS);
         double used = duration / jwtProperties.getRefreshTokenExpiration();
-        log.info("Refresh token duration: {}", duration);
-        log.info("Refresh token used: {}", used);
-        return !(used < extendThreshold);
+
+        return (1 - used) < extendThreshold;
     }
 }

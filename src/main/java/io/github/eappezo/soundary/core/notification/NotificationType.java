ad84a8e63b55f0bb671ccca09252d7b45922330a
Gyumin Bae <bgmsound0528@gmail.com>
2024-11-06 22:41:09 +0900

    hotfix: notice 타입 명시

--- src/main/java/io/github/eappezo/soundary/core/notification/NotificationType.java
+++ src/main/java/io/github/eappezo/soundary/core/notification/NotificationType.java
@@ -4,13 +4,18 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum NotificationType {
-    MUSIC_SHARED("N0001"),
-    RECEIVED_FRIEND_REQUEST("N0002"),
-    ACCEPTED_FRIEND_REQUEST("N0003");
+    MUSIC_SHARED("N0001", false),
+    RECEIVED_FRIEND_REQUEST("N0002", true),
+    ACCEPTED_FRIEND_REQUEST("N0003", true),;
 
     private final String code;
+    private final Boolean isForeground;
 
     public String code() {
         return code;
+    }
+
+    public Boolean isBackground() {
+        return !isForeground;
     }
 }

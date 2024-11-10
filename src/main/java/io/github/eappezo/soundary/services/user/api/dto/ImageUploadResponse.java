8d22314f6af8cad25f10e20e6fd2a48097acc0cc
Gyumin Bae <bgmsound0528@gmail.com>
2024-11-10 16:25:25 +0900

    refactor: 이미지 업로드 리턴값 변경

--- src/main/java/io/github/eappezo/soundary/services/user/api/dto/ImageUploadResponse.java
+++ src/main/java/io/github/eappezo/soundary/services/user/api/dto/ImageUploadResponse.java
@@ -1,11 +1,16 @@
 package io.github.eappezo.soundary.services.user.api.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.github.eappezo.soundary.core.persistence.Image;
 
+@JsonNaming(SnakeCaseStrategy.class)
 public record ImageUploadResponse(
-        String id
+        String uploadedImageUrl
 ) {
     public static ImageUploadResponse from(Image image) {
-        return new ImageUploadResponse(image.id().toString());
+        return new ImageUploadResponse(
+                String.format("https://api.soundary.kro.kr/api/v1/images/%s", image.id().toString())
+        );
     }
 }

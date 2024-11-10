7b45a00420fb58b7939dc23bc115df15b56131a6
Gyumin Bae <bgmsound0528@gmail.com>
2024-11-10 10:58:49 +0900

    feat: 친구 조회 시 라벨도 같이 조회

--- src/main/java/io/github/eappezo/soundary/services/friend/application/dto/FriendInfo.java
+++ src/main/java/io/github/eappezo/soundary/services/friend/application/dto/FriendInfo.java
@@ -1,19 +1,16 @@
 package io.github.eappezo.soundary.services.friend.application.dto;
 
 import io.github.eappezo.soundary.core.identification.Identifier;
+import io.github.eappezo.soundary.core.user.Label;
 import io.github.eappezo.soundary.core.user.User;
+
+import java.util.List;
 
 public record FriendInfo(
     Identifier id,
     String displayId,
     String nickname,
-    String profileImageUrl
+    String profileImageUrl,
+    List<Label> labels
 ) {
-    public static FriendInfo from(User user) {
-        return new FriendInfo(
-            user.getIdentifier(),
-            user.getDisplayId(),
-            user.getNickname(),
-            user.getProfileImageUrl());
-    }
 }
